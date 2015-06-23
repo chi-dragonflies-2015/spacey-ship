f1f26daec801cb58023145ea3ce78622df6b472c
Henry Firth <henrynf12@gmail.com>
2015-06-22 23:20:58 -0500

    Make ship orbit center of page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,30 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+var t = 0;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+function moveit() {
+    t += 0.05;
+
+    shipRadius = $('#ship').width()/2;
+
+    var xCenter = $(window).width() / 2;   // center X position
+    var yCenter = $(window).height() / 2;   // center Y position
+
+    var radius = yCenter * 0.75;
+
+    if (yCenter > xCenter) {
+      radius = xCenter * 0.75;
+    }
+
+    var newLeft = Math.floor(xCenter - shipRadius + (radius * Math.cos(t)));
+    var newTop = Math.floor(yCenter - shipRadius + (radius * Math.sin(t)));
+
+    $('#ship').animate({
+        top: newTop,
+        left: newLeft,
+    }, 1, function() {
+        moveit();
+    });
+}
+
+$(document).ready(function() {
+  moveit();
 });
