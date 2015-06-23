86049e9d67e65d43d70c5de38551f2871fe10952
Henry Firth <henrynf12@gmail.com>
2015-06-23 16:44:22 -0500

    Add realistic gravity and spacebar interaction

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
-var t = 0;
-var g = 0;
+var angle = 0;
+var r = 0;
 
 function collision() {
   $hole = $('#black-hole')
@@ -22,57 +22,48 @@ function collision() {
   return true;
 }
 
-function moveit(g) {
+function moveit(v) {
+  var G = 10;
+  var D = 0.06;
+
   if (collision()) {
-    alert("Game Over!");
+    var orbits = Math.floor(angle / (Math.PI * 2));
+    alert("Game Over! You completed " + orbits + " orbits!");
     return;
   }
-  t += 0.025;
-  g += 0.05;
+
+  angle += 0.06;
 
   shipSize = $('#ship').width();
 
   var xCenter = $(window).width() / 2 - shipSize;
   var yCenter = $(window).height() / 2 - shipSize;
 
+  $(document).on('keyup', function(event) {
+    if (event.keyCode == 32) {
+      v = -30;
+    }
+  });
+
+  v += G*D;
+  r += v*D;
+
   var radius = yCenter * 0.75;
-
-  radius = radius - Math.pow(g,2)
-
   if (yCenter > xCenter) {
     radius = xCenter * 0.75;
   }
 
-  var newLeft = Math.floor(xCenter + (radius * Math.cos(t)));
-  var newTop = Math.floor(yCenter  + (radius * Math.sin(t)));
+  radius = radius - r;
+
+  var newLeft = Math.floor(xCenter + (radius * Math.cos(angle)));
+  var newTop = Math.floor(yCenter  + (radius * Math.sin(angle)));
 
   $('#ship').animate({
       top: newTop,
       left: newLeft,
   }, 1, function() {
-      moveit(g);
+      moveit(v);
   });
-}
-
-function collision() {
-  $hole = $('#black-hole')
-  $ship = $('#ship')
-
-  var x1 = $ship.offset().left;
-  var y1 = $ship.offset().top;
-  var h1 = $ship.outerHeight(true);
-  var w1 = $ship.outerWidth(true);
-  var b1 = y1 + h1;
-  var r1 = x1 + w1;
-  var x2 = $hole.offset().left;
-  var y2 = $hole.offset().top;
-  var h2 = $hole.outerHeight(true);
-  var w2 = $hole.outerWidth(true);
-  var b2 = y2 + h2;
-  var r2 = x2 + w2;
-
-  if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-  return true;
 }
 
 $(document).ready(function() {
