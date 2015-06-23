b603fbe1c85388058823e6f518211cc7dac7eaf3
Henry Firth <henrynf12@gmail.com>
2015-06-22 21:50:44 -0500

    Add seed file

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+30.times do
+  user = User.create(username: Faker::Internet.user_name)
+  5.times do
+    user.games << Game.create(score: rand(30)+1)
+  end
+  user.highscore = user.games.map {|game| game.score}.max
+  user.save
+end
