a45f148c9535d93e6669fe8784cbb1fd1e3700fe
Henry Firth <henrynf12@gmail.com>
2015-06-25 18:04:27 -0500

    Implement bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_many :games, foreign_key: :player_id
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password_attempt)
+    user = User.find_by(email: email)
+    return user if user && user.password = password_attempt
+  end
 end
