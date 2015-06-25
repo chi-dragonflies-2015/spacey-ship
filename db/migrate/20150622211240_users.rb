e4243444774ff8bc9991c593c7ea589d2cce246d
Henry Firth <henrynf12@gmail.com>
2015-06-25 18:06:38 -0500

    Set defaut highscore in server

--- db/migrate/20150622211240_users.rb
+++ db/migrate/20150622211240_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, uniqueness: :true
-      t.integer :highscore
+      t.integer :highscore, default: 0
 
       t.timestamps null: false
     end
